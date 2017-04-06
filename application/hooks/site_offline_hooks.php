3c676317cef2b5aa1b4abad3c7af080c86a63ccf
Hekkydev <nhekky@gmail.com>
2017-04-06 16:56:35 +0700

    Menambahkan Fitur Maintenance

--- application/hooks/site_offline_hooks.php
+++ application/hooks/site_offline_hooks.php
@@ -4,18 +4,37 @@
  * Site_offline_hooks
  * Author : Hekky Nurhikmat
  */
-class site_offline_hooks 
-{
-    var $CI;
-    function __construct()
-    {
-      $this->CI =& get_instance();  
-    }
+ class site_offline_hooks {
 
-    function is_offline()
-    {
-        if (file_exists(APPPATH.'config/config.php')) {
-          
-        }
-    }
-}
+     public function __construct()
+     {
+        log_message('debug','Accessing site_offline hook!');
+     }
+
+     public function is_offline()
+     {
+       if(file_exists(APPPATH.'config/config.php'))
+       {
+           include(APPPATH.'config/config.php');
+
+           if(isset($config['is_offline']) && $config['is_offline']===TRUE)
+           {
+             $ip_maintenance = $config['ip_maintenance'];
+             if(in_array($_SERVER['REMOTE_ADDR'],$ip_maintenance,TRUE)){
+               
+             }else{
+               $this->show_site_offline();
+               exit;
+             }
+
+
+           }
+       }
+     }
+
+     private function show_site_offline()
+     {
+          include('others/maintenance.php');
+     }
+
+ }
